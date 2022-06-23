734ab68ebae7132e52a8a1d6417bfaa845a7527c
NursTLS <107918147+NursTLS@users.noreply.github.com>
2022-06-23 09:47:40 +0600

    Add logic of game

---
+++ src/TicTacToe.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+    public TicTacToe() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
+        printTableMaking();
+        char[][] gameTable = new char[][]{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        if ((new Random()).nextBoolean()) {
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+            System.out.println("Computer made a move.");
+        }
+
+        while(true) {
+            makeUserMove(gameTable);
+            printGameTable(gameTable);
+            if (userWin(gameTable)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+
+            if (isDraw(gameTable)) {
+                System.out.println("SORRY, DRAW!");
+                break;
+            }
+
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+            System.out.println("Computer made a move.");
+            if (computerWin(gameTable)) {
+                System.out.println("COMPUTER WIN!");
+                break;
+            }
+
+            if (isDraw(gameTable)) {
+                System.out.println("SORRY, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
+    }
+
+    private static void printTableMaking() {
+    }
+
+    private static void makeComputerMove(char[][] gameTable) {
+    }
+
+    private static void printGameTable(char[][] gameTable) {
+    }
+
+    private static void makeUserMove(char[][] gameTable) {
+    }
+
+    private static boolean userWin(char[][] gameTable) {
+        return true;
+    }
+
+    private static boolean computerWin(char[][] gameTable) {
+        return true;
+    }
+
+    private static boolean isDraw(char[][] gameTable) {
+        return true;
+    }
+}
