280eea51e87d7cf68d538dba059488508e7499a8
NursTLS <107918147+NursTLS@users.noreply.github.com>
2022-06-23 09:49:45 +0600

    Add printTableMaking function

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -45,6 +45,8 @@ public class TicTacToe {
     }
 
     private static void printTableMaking() {
+        char[][] mappingTable = new char[][]{{'7', '8', '9'}, {'4', '5', '6'}, {'1', '2', '3'}};
+        printGameTable(mappingTable);
     }
 
     private static void makeComputerMove(char[][] gameTable) {
